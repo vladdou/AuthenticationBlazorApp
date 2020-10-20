4c6a546b2c45ebf05f8c668c244f17c7bf850ef2
Vladimir <vladdou@users.noreply.github.com>
2020-10-20 12:02:51 -0700

    Add project files.

---
+++ AuthenticationBlazorApp/Provider/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AuthenticationBlazorApp.Provider
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+            new Claim(ClaimTypes.Name, "testUser"),
+            }, "TestUser authentication type");
+            var user = new ClaimsPrincipal(identity);
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
